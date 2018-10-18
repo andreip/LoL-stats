201d78355e0fbb052b773429d98a58db7757ca74
Andrei Petre <p31andrei@gmail.com>
2018-10-17 23:40:57 -0700

    Add simple logic to print all match ids of a player.

--- src/public/js/lol_stats.js
+++ src/public/js/lol_stats.js
@@ -1,23 +1,113 @@
 'use strict';
 
-class App extends React.Component {
+const SearchForm = ({ doSearch }) => {
+    let input;
+
+    return (
+        <div>
+            <input ref={node => {input = node;}} />
+            <button onClick={() => {
+                doSearch(input.value);
+                input.value = '';
+            }}>
+                Search
+            </button>
+        </div>
+    );
+};
+
+const LoLMatch = ({ summonerName, gameId  }) => {
+    return (
+        <li>
+            <div>{summonerName}: match with id {gameId}</div>
+        </li>
+    );
+};
+
+const LoLTitle = ({ name }) => {
+    if (name) {
+        return (
+            <h2>Matches for {name}</h2>
+        );
+    } else {
+        return (
+            <h2>No matches</h2>
+        );
+    }
+};
+
+const LoLMatchList = ({ searching, summonerName, matches }) => {
+    if (searching) {
+        return <h2>Searching matches for {summonerName}...</h2>;
+    }
+
+    const nodes = matches.map((match, idx) => {
+        return (
+            <LoLMatch key={idx} summonerName={summonerName} {...match} />
+        );
+    });
+
+    return (
+        <div>
+            <LoLTitle name={summonerName} />
+            <ul>{nodes}</ul>
+        </div>
+    );
+};
+
+class LoLStatsApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = {
+        summonerName: '',
+        matches: [],
+        searching: false,
+    };
+    console.log("initial state", this.state);
+  }
+
+  doSearch(summonerName) {
+    this.setState({ searching: true, matches: [], summonerName });
+    // simulates a computation
+    console.log("Fetching data...");
+    setTimeout(() => {
+        console.log("Done fetching data.");
+        console.log("DATA", DATA);
+        this.setState({ searching: false, ...DATA });
+    }, 2000);
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
     return (
-        <button onClick={() => this.setState({ liked: true })}>
-            Like
-        </button>
+        <div className="lol-stats">
+            <SearchForm doSearch={this.doSearch.bind(this)} />
+            <LoLMatchList {...this.state} />
+       </div>
     );
   }
 }
 
 const domContainer = document.querySelector('#lol-stats-container');
-ReactDOM.render(<App/>, domContainer);
+ReactDOM.render(<LoLStatsApp />, domContainer);
+
+// TODO: eventually fetch it from server.
+const DATA = {
+    summonerName: "TF Blade",
+    matches: [
+        {
+            gameId: 2888919284,
+            platformId: "NA1",
+            gameCreation: 1539790867247,
+            gameDuration: 1487,
+            queueId: 420,
+            mapId: 11,
+            seasonId: 11,
+            gameVersion: "8.20.249.1952",
+            gameMode: "CLASSIC",
+            gameType: "MATCHED_GAME",
+            teams: [],
+            participants: [],
+            participantIdentities: []
+        }
+    ]
+};
