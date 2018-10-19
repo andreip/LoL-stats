601367aee786cc4a5faa6537e825c6f20b98294f
Andrei Petre <p31andrei@gmail.com>
2018-10-19 07:58:28 -0700

    Move code to a class and break down into methods better.

--- src/index.js
+++ src/index.js
@@ -13,9 +13,6 @@ if (!LOL_API_KEY) {
     throw new Error("Please setup a valid LOL_API_KEY env variable (https://developer.riotgames.com/).");
 }
 
-const app = express();
-const port = 3000;
-
 // hardcoded data for LOL
 const baseURL = 'https://na1.api.riotgames.com/';
 const getMatchesForAccountURL = accountId => `/lol/match/v3/matchlists/by-account/${accountId}`;
@@ -23,80 +20,83 @@ const getMatchURL = matchId => `/lol/match/v3/matches/${matchId}`;
 const getSummonerURL = summonerName => `/lol/summoner/v3/summoners/by-name/${summonerName}`;
 const maxRecentGames = 5;  // number of most games to fetch
 
-function buildURI(path, base, apikey) {
-    const url = new URL(path, base);
-    url.searchParams.append('api_key', apikey);
-    return url;
-};
 
+class App {
+    constructor(port=3000) {
+        this.port = port;
+        this.app = express();
+        this.app.use(express.static(join(__dirname, 'public')));
 
+        this.app.get('/', this.indexPage());
+        this.app.get('/summoner/:summonerName/most-recent-matches/fake', this.fakeMatches());
+        this.app.get('/summoner/:summonerName/most-recent-matches', this.mostRecentMatches());
+        this.app.listen(port,
+            () => console.log(`Example app listening on port ${port}`));
+    }
 
-app.use(express.static(join(__dirname, 'public')));
+    indexPage() {
+        return (req, res) => {
+            res.sendFile(join(__dirname, '/public/index.html'));
+        }
+    }
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, '/public/index.html'));
-});
+    fakeMatches() {
+        return (req, res) => {
+            res.json({
+                summonerName: "TF Blade",
+                accountId: 202988570,
+                matches: [FAKE_MATCH]
+            });
+        }
+    }
 
-app.get('/summoner/:summonerName/most-recent-matches/fake', (req, res) => {
-    res.json({
-        summonerName: "TF Blade",
-        matches: [FAKE_MATCH]
-    });
-});
+    mostRecentMatches() {
+        return (req, outerRes) => {
+            const { summonerName } = req.params;
+            console.log(`Looking for summoner name ${summonerName}`);
 
-app.get('/summoner/:summonerName/most-recent-matches', (req, outerRes) => {
-    const { summonerName } = req.params;
-    console.log(`Looking for summoner name ${summonerName}`);
+            // check user's input before API call.
+            if (!summonerNameRegex.test(summonerName)) {
+                outerRes.send({err: `Invalid summoner name ${summonerName}`});
+                return;
+            }
 
-    if (!summonerNameRegex.test(summonerName)) {
-        outerRes.send({err: `Invalid summoner name ${summonerName}`});
-        return;
+            const config = {params: {api_key: LOL_API_KEY}, baseURL};
+            this.fetchAccountInfo({outerRes, summonerName, config});
+        }
     }
 
     //
     // first do API call to find out summoner's account id.
     //
-    const config = {params: {api_key: LOL_API_KEY}, baseURL};
-    axios.get(getSummonerURL(summonerName), config)
-    .then(({data: account}) => {
-        console.log("Fetched account", account);
-        const accountId = account.accountId;
+    fetchAccountInfo({outerRes, summonerName, config}) {
+        axios.get(getSummonerURL(summonerName), config)
+        .then(({data: account}) => {
+            const accountId = account.accountId;
+            this.fetchListOfMatches({summonerName, outerRes, accountId, config});
+        }).catch(e => {
+            // of the range of 2xx
+            if (e.response) {
+                console.log(`Couldn't get ${summonerName} account; status`, e.response.status);
+                outerRes.send({err: `Account for ${summonerName} was not found.`});
+            } else {
+                console.log("Error fetching account", e);
+                outerRes.send({err: `Failed to fetch account info for ${summonerName}`});
+            }
+        });
+    }
 
-        //
-        // now do API call to get latest matches for accountId
-        //
+    //
+    // now do API call to get latest matches for accountId
+    //
+    fetchListOfMatches(params) {
+        let {summonerName, accountId, matches, config} = params;
         axios.get(getMatchesForAccountURL(accountId),
                   R.mergeDeepLeft(config, {params: {endIndex: maxRecentGames}}))
         .then(r => {
             const matchIds = r.data.matches.map(R.prop('gameId'));
             console.log(`Got ${matchIds.length} most recent match ids.`);
-
-            //
-            // now fetch info for all games and bundle them together to send
-            // to frontend.
-            //
-            let promises = matchIds.map(id => {
-                return axios.get(getMatchURL(id), config)
-                  .catch(e => {
-                    if (e.response) {
-                        console.log(`Could not fetch matchId ${id} for ${summonerName}; status`, e.response.status);
-                    } else {
-                        console.log(`Failed for matchId ${id} for ${summonerName}`, e);
-                    }
-                  });
-            });
-            axios.all(promises)
-            .then(responses => {
-                // for some reason axios returns all responses here where some
-                // are empty (the failed ones).
-                const matches = R.pipe(
-                    R.map(R.prop('data')),
-                    R.reject(R.isEmpty),
-                    R.reject(R.isNil),
-                )(responses);
-                console.log(`Successfully fetched ${matches.length}/${matchIds.length} matches.`);
-                outerRes.send({summonerName, accountId, matches});
-            });
+            this.fetchMatchDetails({matchIds, ...params});
         }).catch(e => {
             // of the range of 2xx
             if (e.response) {
@@ -107,16 +107,39 @@ app.get('/summoner/:summonerName/most-recent-matches', (req, outerRes) => {
                 outerRes.send({err: `Failed to fetch matches for ${summonerName}`});
             }
         });
-      }).catch(e => {
-        // of the range of 2xx
-        if (e.response) {
-            console.log(`Couldn't get ${summonerName} account; status`, e.response.status);
-            outerRes.send({err: `Account for ${summonerName} was not found.`});
-        } else {
-            console.log("Error fetching account", e);
-            outerRes.send({err: `Failed to fetch account info for ${summonerName}`});
-        }
-      });
-});
+    }
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+    //
+    // now fetch info for all games and bundle them together to send
+    // to frontend.
+    //
+    fetchMatchDetails({summonerName, outerRes, matchIds, config, accountId}) {
+        let promises = matchIds.map(id => {
+            return axios.get(getMatchURL(id), config)
+              .catch(e => {
+                if (e.response) {
+                    console.log(`Could not fetch matchId ${id} for ${summonerName}; status`, e.response.status);
+                } else {
+                    console.log(`Failed for matchId ${id} for ${summonerName}`, e);
+                }
+              });
+        });
+        axios.all(promises)
+        .then(responses => {
+            console.log("After Fetched match details");
+            // for some reason axios returns all responses here where some
+            // are empty (the failed ones).
+            const matches = R.pipe(
+                R.map(R.prop('data')),
+                R.reject(R.isEmpty),
+                R.reject(R.isNil),
+            )(responses);
+            console.log(`Successfully fetched ${matches.length}/${matchIds.length} matches.`);
+            outerRes.send({summonerName, accountId, matches});
+        });
+    }
+}
+
+if (require.main == module) {
+    new App();
+}
